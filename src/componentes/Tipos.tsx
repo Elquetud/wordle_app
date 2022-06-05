a1b906bbf806a2115d83f65332a162c8589712c4
Elquetud <jesusromeroarmenta@outlook.com>
2022-06-05 02:02:39 -0600

    caja_de_casillas_de_letras_y_estados_de_variables

---
+++ src/componentes/Tipos.tsx
@@ -0,0 +1,10 @@
+export type cajastatus = "ausente" | "presente" | "correcto" | "vacío" | "editar";
+
+export const enum GameStatus {
+  jugando,
+  gano,
+  perdio,
+
+}
+
+
---
+++ src/componentes/Wordle.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Wordle = () => {
+    
+  return (
+    <div>Hola wordle</div>
+    
+  )
+
+}
+
+export default Wordle;
