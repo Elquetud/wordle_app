d8b3dbacf7b6cd1b1ee92c3d2f508e98b9cccd15
Elquetud <jesusromeroarmenta@outlook.com>
2022-06-08 00:15:26 -0600

    validar letra correcta e implementacion de la funcion dentro del arreglo

--- src/componentes/FilaCompletada.tsx
+++ src/componentes/FilaCompletada.tsx
@@ -1,5 +1,5 @@
 import Caja from './Caja'
-
+import { cajastatus } from "./Tipos";
 
 interface PropiedadesFila {
    casilla:string,
@@ -8,11 +8,29 @@ interface PropiedadesFila {
 
 export const FilaCompletada = ({casilla,solucion}:PropiedadesFila) => {
 
+   function comprobarLetra(letter: string, pos: number): cajastatus {
+      if (solucion.includes(letter)) {
+        if (solucion[pos] === letter) {
+          return "correcto";
+        } else {
+          return "presente";
+        }
+      } else {
+        return "vacio";
+      }
+    }
+
   return (
+     
     <div>
        {
           Array.from((Array(5))).map((_,i)=>(
-             <Caja key={i} valor={"a"} estado={'correcto'} />      
+             <Caja 
+             key={i} 
+             valor={"a"} 
+             estado={'correcto'}
+             
+             />      
 
           ))
 
