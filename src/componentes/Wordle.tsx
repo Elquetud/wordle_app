98631d22c21bd14ea548322e1a2bde0d505b9c79
Elquetud <jesusromeroarmenta@outlook.com>
2022-06-09 01:23:16 -0600

    estados del juego (palabra,turno,palabracompleted y estado del juego actual)

--- src/componentes/Wordle.tsx
+++ src/componentes/Wordle.tsx
@@ -1,9 +1,20 @@
+import { useState,useEffect } from 'react';
 import FilaActual from './FilaActual';
 import { FilaCompletada } from './FilaCompletada';
 import FilaVacia from './FilaVacia';
+import {GameStatus} from './Tipos';
 
 export const Wordle = () => {
-    
+  const [palabra, setPalabra] = useState<string>("");
+  const [turno, setTurno] = useState<number>(1);
+  const [currentPalabra, setCurrentPalabra] = useState<string>("");
+  const [completedPalabra, setCompletedPalabra] = useState<string[]>([]);
+  const [estadoJuego, setestadoJuego] = useState<GameStatus>(GameStatus.jugando);
+  
+  useEffect(() => {
+    setPalabra("break");
+  }, []);
+
   return (
   
     <div>
